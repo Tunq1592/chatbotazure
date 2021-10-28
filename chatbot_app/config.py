f7e3ccb8e9e2fe4129ca1fd482f44b31bb3a5d37
tu.nguyenq <tu.nguyenq@homecredit.vn>
2021-10-28 15:18:14 +0700

    update docker compose

--- chatbot_app/config.py
+++ chatbot_app/config.py
@@ -10,3 +10,5 @@ class DefaultConfig:
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "444217b8-a823-493e-8ee2-afd07ccc796d")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "5T57Q~dl8p7hnRfx~jokpGzi.b_QoaIg-LOZs")
+    # APP_ID = os.environ.get("MicrosoftAppId", "")
+    # APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
